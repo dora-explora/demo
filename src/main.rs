a18560dc1c6585d5db257f8b02a943352c88b81e
dora-explora <sarrafdara@gmail.com>
2025-09-08 22:39:13 -0700

    started on rainbow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
-use std::io::{Result, Stdout, stdout, BufReader};
+use std::io::{Result, stdout, BufReader};
 use rodio::{OutputStreamBuilder, play};
-use crossterm::{cursor, terminal, ExecutableCommand};
+use crossterm::{cursor, terminal, ExecutableCommand, style::{self, Stylize, Color}};
 
 fn main() -> Result<()> {
 	let stream_handle = OutputStreamBuilder::open_default_stream()
@@ -10,15 +10,40 @@ fn main() -> Result<()> {
 	let sink = play(&stream_handle.mixer(), file).unwrap();
 
 	let mut stdout = stdout();
-    stdout.execute(terminal::EnterAlternateScreen)?;
-    terminal::enable_raw_mode()?;
-    stdout.execute(cursor::Hide)?;
-    stdout.execute(terminal::DisableLineWrap)?;
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    // stdout.execute(terminal::EnterAlternateScreen)?;
+    // terminal::enable_raw_mode()?;
+    // stdout.execute(cursor::Hide)?;
+    // stdout.execute(terminal::DisableLineWrap)?;
+    // stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
-	sink.sleep_until_end();
+	for i in 0..85 {
+        stdout.execute(style::PrintStyledContent(' '.on(Color::Rgb { r: 255, g: i * 3, b: 0 })))?;
+    }
+    // stdout.execute(cursor::MoveToNextLine(1))?;
+	for i in 0..85 {
+        stdout.execute(style::PrintStyledContent(' '.on(Color::Rgb { r: 255 - i * 3, g: 255, b: 0 })))?;
+    }
+    // stdout.execute(cursor::MoveToNextLine(1))?;
+	for i in 0..85 {
+        stdout.execute(style::PrintStyledContent(' '.on(Color::Rgb { r: 0, g: 255, b: i * 3 })))?;
+    }
+    // stdout.execute(cursor::MoveToNextLine(1))?;
+	for i in 0..85 {
+        stdout.execute(style::PrintStyledContent(' '.on(Color::Rgb { r: 0, g: 255 - i * 3, b: 255 })))?;
+    }
+    // stdout.execute(cursor::MoveToNextLine(1))?;
+	for i in 0..85 {
+        stdout.execute(style::PrintStyledContent(' '.on(Color::Rgb { r: i * 3, g: 0, b: 255 })))?;
+    }
+    // stdout.execute(cursor::MoveToNextLine(1))?;
+	for i in 0..85 {
+        stdout.execute(style::PrintStyledContent(' '.on(Color::Rgb { r: 255, g: 0, b: 255 - i * 3 })))?;
+    }
+    // stdout.execute(cursor::MoveToNextLine(1))?;
 
-    terminal::disable_raw_mode()?;
+    // sink.sleep_until_end();
+
+    // terminal::disable_raw_mode()?;
     println!("bye bye");
     return Ok(());
 }
